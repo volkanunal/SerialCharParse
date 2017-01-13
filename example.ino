14affbbb35d6d65a993de2b7362f0cc167bcf42d
Volkan Unal <engineeringvolkan@hotmail.com>
2017-01-14 01:58:03 +0200

    Update example.ino

--- example.ino
+++ example.ino
@@ -3,26 +3,17 @@ char veri_girdisi[100];
 int i=0;
 char gelen;
 void setup()
-{
-  Serial.begin(9600);
-  
-}
+{Serial.begin(9600);}
 void loop()
 {
-  if(Serial.available()>0)
-  {
+  if(Serial.available()>0){
    gelen=Serial.read();
    veri_girdisi[i]=gelen;
-   i++;
-   }
+   i++;}
  if(gelen=='\n' && i) {
  Serial.println( veri_ayir('A',veri_girdisi,i));
  Serial.println( veri_ayir('B',veri_girdisi,i));
 for(int a=0; a<100; a++) {
  veri_girdisi[a]=0;
- i=0;
- }
- }
- 
-}
+ i=0;}}}
 
