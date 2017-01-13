b3c40370b0d8dbd628316349e1ed3e0f39c3b0f9
Volkan Unal <engineeringvolkan@hotmail.com>
2017-01-14 01:16:11 +0200

    Add files via upload

---
+++ VU_Serial.h
@@ -0,0 +1,31 @@
+double veri_ayir(char aranan,char buffer[],int buffer_size)
+{
+ char gecici_buffer[10]="";
+int gecici_buffer_deger=0;
+int aranan_durum;
+for(byte i=0; i<buffer_size; i++)
+{
+ 
+ {
+  if (buffer[i] == aranan)
+     {
+     i++;
+     while(i<buffer_size && gecici_buffer_deger<10)
+     {
+  //    if(buffer[i]==0 || buffer[i]==' ')
+   //   break;
+     gecici_buffer[gecici_buffer_deger]=buffer[i];
+     i++;
+     gecici_buffer_deger++;
+     }
+     return strtod(gecici_buffer,NULL);
+   
+     }
+   
+    
+}
+
+}
+return 0;
+}
+
