ac22c1896a6be998b5cb5a4436e3cd3c62b9dbce
Volkan Unal <engineeringvolkan@hotmail.com>
2017-01-14 02:00:47 +0200

    Update VU_Serial.h

--- VU_Serial.h
+++ VU_Serial.h
@@ -1,26 +1,17 @@
-double veri_ayir(char aranan,char buffer[],int buffer_size)
-{
- char gecici_buffer[10]="";
+double veri_ayir(char aranan,char buffer[],int buffer_size){
+char gecici_buffer[10]="";
 int gecici_buffer_deger=0;
-for(byte i=0; i<buffer_size; i++)
+for(byte i=0; i<buffer_size; i++){
 {
- 
- {
-  if (buffer[i] == aranan)
-     {
+  if (buffer[i] == aranan) {
      i++;
-     while(i<buffer_size && gecici_buffer_deger<10)
-     {
+     while(i<buffer_size && gecici_buffer_deger<10){
      if(buffer[i]==0 || buffer[i]==' ')
       break;
      gecici_buffer[gecici_buffer_deger]=buffer[i];
      i++;
      gecici_buffer_deger++;
      }
-     return strtod(gecici_buffer,NULL);
-   
-     }
-  }
-}
+     return strtod(gecici_buffer,NULL);} } }
 return 0;}
 
